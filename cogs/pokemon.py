69781c11a29bf010299ca43131b54eb39caad09c
KU13 <lionplays1062@gmail.com>
2021-06-21 14:09:19 -0400

    Pokemon Cog Improvement Update Complete

--- cogs/pokemon.py
+++ cogs/pokemon.py
@@ -18,92 +18,88 @@ class Pokemon(commands.Cog):
     async def on_ready(self):
         print(self.qualified_name)
 
-    @commands.group(aliases=['p'])
-    async def pokemon(self, ctx):
+    @commands.group(aliases=['p'], invoke_without_command=True)
+    async def pokemon(self, ctx, gen=None, lives=None):
         if not ctx.invoked_subcommand:
-            await ctx.invoke(self.bot.get_command('pokemon whosthatpokemon'))
+            if ctx.channel.id == 855122761071984660:
+                IsGen = False
+                f = 1, 898
+                if gen:
+                    try: 
+                        gen = int(gen)
+                        if gen <= 8:
+                            IsGen = True
+                            if gen == 1:
+                                f = 1, 151
+                            elif gen == 2:
+                                f = 152, 251
+                            elif gen == 3:
+                                f = 252, 386
+                            elif gen == 4:
+                                f = 387, 493
+                            elif gen == 5:
+                                f = 494, 649
+                            elif gen == 6:
+                                f = 650, 721
+                            elif gen == 7:
+                                f = 722, 809
+                            elif gen == 8:
+                                f = 810, 898
+                    except:
+                        pass
+                e = random.randint(f[0],f[1])
+                page = requests.get(f"https://www.pokemon.com/us/pokedex/{e}")
+                
+                        
+                tree = html.fromstring(page.content)
+                pName = tree.xpath('/html/body/div[4]/section[1]/div[2]/div/text()')[0].strip()
+                if e == 32 or e == 29:
+                    pName = "Nidoran"
+                pImg = tree.xpath('/html/body/div[4]/section[3]/div[1]/div[1]/div/img')[0].attrib.get('src')
 
-    @pokemon.command()
-    async def whosthatpokemon(self, ctx, gen=None, lives=None):
-        if ctx.channel.id == 855122761071984660:
-            IsGen = False
-            f = 1, 898
-            if gen:
-                try: 
-                    gen = int(gen)
-                    if gen <= 8:
-                        IsGen = True
-                        if gen == 1:
-                            f = 1, 151
-                        elif gen == 2:
-                            f = 152, 251
-                        elif gen == 3:
-                            f = 252, 386
-                        elif gen == 4:
-                            f = 387, 493
-                        elif gen == 5:
-                            f = 494, 649
-                        elif gen == 6:
-                            f = 650, 721
-                        elif gen == 7:
-                            f = 722, 809
-                        elif gen == 8:
-                            f = 810, 898
-                except:
-                    pass
-            e = random.randint(f[0],f[1])
-            page = requests.get(f"https://www.pokemon.com/us/pokedex/{e}")
-            
-                    
-            tree = html.fromstring(page.content)
-            pName = tree.xpath('/html/body/div[4]/section[1]/div[2]/div/text()')[0].strip()
-            if e == 32 or e == 29:
-                pName = "Nidoran"
-            pImg = tree.xpath('/html/body/div[4]/section[3]/div[1]/div[1]/div/img')[0].attrib.get('src')
+                #Image Conversion
+                img = Image.open(requests.get(pImg, stream=True).raw)
+                img = img.convert("RGBA")
+                datas = img.getdata()
 
-            #Image Conversion
-            img = Image.open(requests.get(pImg, stream=True).raw)
-            img = img.convert("RGBA")
-            datas = img.getdata()
+                newData = []
+                for item in datas:
+                    if item[3] != 0:
+                        newData.append((0,0,0))
+                    else:
+                        newData.append(item)
+                img.putdata(newData)
+                img.save("pokemon.png", "PNG")
 
-            newData = []
-            for item in datas:
-                if item[3] != 0:
-                    newData.append((0,0,0))
+                #Message Send
+                a = "Who's That Pokémon?"
+                f = ""
+                l = ""
+                if IsGen:
+                    f = f" | Generation {gen} Edition"
+                if lives:
+                    a = "Auto Pokémon"
+                    l = f" | Lives Left: {lives}"
+                embed=discord.Embed(title="Pokémon", description=f"{a}{f}{l}", color=0xffcb05)
+                embed.set_image(url="attachment://pokemon.png")
+                msg = await ctx.send(file=discord.File("pokemon.png"), embed=embed)
+
+                def check(m):
+                    return m.content.lower() == pName.lower()
+                r = False
+                try:
+                    await self.bot.wait_for('message', check=check, timeout=config['pokemon_cooldown'])
+                except asyncio.TimeoutError:
+                    embed.set_footer(text=f"Your {config['pokemon_cooldown']} seconds ran out, the correct answer was {pName}.")
+                    r = True
                 else:
-                    newData.append(item)
-            img.putdata(newData)
-            img.save("pokemon.png", "PNG")
+                    embed.set_footer(text="Correct.")
 
-            #Message Send
-            a = "Who's That Pokémon?"
-            f = ""
-            l = ""
-            if IsGen:
-                f = f" | Generation {gen} Edition"
-            if lives:
-                a = "Auto Pokémon"
-                l = f" | Lives Left: {lives}"
-            embed=discord.Embed(title="Pokémon", description=f"{a}{f}{l}", color=0xffcb05)
-            embed.set_image(url="attachment://pokemon.png")
-            msg = await ctx.send(file=discord.File("pokemon.png"), embed=embed)
-
-            def check(m):
-                return m.content.lower() == pName.lower()
-            r = False
-            try:
-                await self.bot.wait_for('message', check=check, timeout=config['pokemon_cooldown'])
-            except asyncio.TimeoutError:
-                embed.set_footer(text=f"Your {config['pokemon_cooldown']} seconds ran out, the correct answer was {pName}.")
-                r = True
-            else:
-                embed.set_footer(text="Correct.")
-
-            embed.set_image(url=pImg)
-            await msg.edit(embed=embed, attachments=None)
-            
-            if r:
-                return True
+                embed.set_image(url=pImg)
+                await msg.edit(embed=embed, attachments=None)
+                
+                if r:
+                    return True
 
     @pokemon.command(aliases=['h'])
     async def help(self, ctx):
@@ -111,12 +107,13 @@ class Pokemon(commands.Cog):
 
     @pokemon.command(aliases=['a'])
     async def auto(self, ctx, lives: int=3, gen: int=None):
-        if lives <= 10:
-            while not lives == 0:
-                if await ctx.invoke(self.bot.get_command('pokemon whosthatpokemon'),gen=gen,lives=lives):
-                    lives -= 1
-            embed=discord.Embed(title="Pokémon", description=f"Auto Pokémon | 0 Lives Remaining", color=0xffcb05)
-            await ctx.send(embed=embed)
+        if ctx.channel.id == 855122761071984660:
+            if lives <= 5:
+                while not lives == 0:
+                    if await ctx.invoke(self.bot.get_command('pokemon'),gen=gen,lives=lives):
+                        lives -= 1
+                embed=discord.Embed(title="Pokémon", description=f"Auto Pokémon | 0 Lives Remaining", color=0xffcb05)
+                await ctx.send(embed=embed)
 
     @pokemon.command(aliases=['c'])
     @commands.has_permissions(administrator=True)  
