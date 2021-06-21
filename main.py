8a4818e8ab09e2796fe975b649e0994a2621d0e4
KU13 <lionplays1062@gmail.com>
2021-06-21 09:15:22 -0400

    Update main.py

--- main.py
+++ main.py
@@ -50,7 +50,7 @@ async def rolesSetup(rolesChannel):
 
 @bot.event
 async def on_ready():
-  print(os.listdir())
+  print(os.listdir(), heroku)
   guild = bot.get_guild(848363067616395284)
   #await update("e")
   await rolesSetup(bot.get_channel(854546485664546836))  
